c5b234129ee4d140959a669b4de726f8364cf930
Florencio Balboa Usabiaga <fbusabiaga@gmail.com>
2023-11-16 12:21:04 +0100

    new variable name

--- code/main.py
+++ code/main.py
@@ -15,31 +15,31 @@ if __name__ == '__main__':
   input_file = args.input_file
 
   # Read input file
-  read = FCM.read_input(input_file)
+  parameters = FCM.read_input(input_file)
 
   # Some input variables should be converted to integers or set to default values if they do not exist
-  read['n_steps'] = int(read['n_steps']) if 'n_steps' in read else 1
-  read['n_save'] = int(read['n_save']) if 'n_save' in read else 1
-  read['initial_step'] = int(read['initial_step']) if 'initial_step' in read else 0
-  read['M_system'] = np.array(read['M_system'], dtype=int) if 'M_system' in read else np.zeros(3)
+  parameters['n_steps'] = int(parameters['n_steps']) if 'n_steps' in parameters else 1
+  parameters['n_save'] = int(parameters['n_save']) if 'n_save' in parameters else 1
+  parameters['initial_step'] = int(parameters['initial_step']) if 'initial_step' in parameters else 0
+  parameters['M_system'] = np.array(parameters['M_system'], dtype=int) if 'M_system' in parameters else np.zeros(3)
   
   # Some input variables should be set to default values if they do not exist
-  read['kT'] = read['n_steps'] if 'kT' in read else 0  
+  parameters['kT'] = parameters['n_steps'] if 'kT' in parameters else 0  
     
   # Read particles configuration
-  x = FCM.read_config(read.get('structure'))
+  x = FCM.read_config(parameters.get('structure'))
   
   # Open files
   if True:
-    name = read.get('output_name') + '.config'
+    name = parameters.get('output_name') + '.config'
     config_file = open(name, 'w')
       
   # Time loop
   start_time = time.time()
-  for step in range(read.get('initial_step'), read.get('n_steps')):
+  for step in range(parameters.get('initial_step'), parameters.get('n_steps')):
 
     # Save data
-    if (step % read.get('n_save')) == 0 and step >= 0:    
+    if (step % parameters.get('n_save')) == 0 and step >= 0:    
       print('step = ', step, ', time = ', time.time() - start_time)
       
       # Save configuration
@@ -47,11 +47,11 @@ if __name__ == '__main__':
       np.savetxt(config_file, x)
       
       # Advance time step
-      FCM.advance_time_step(read.get('dt'), read.get('scheme'), step, read)
+      FCM.advance_time_step(parameters.get('dt'), parameters.get('scheme'), step, parameters)
 
     
   # Save last configuration if necessary
-  if ((step+1) % read.get('n_save')) == 0 and step+1 >= 0:    
+  if ((step+1) % parameters.get('n_save')) == 0 and step+1 >= 0:    
     print('step = ', step+1, ', time = ', time.time() - start_time)
 
     # Save configuration
